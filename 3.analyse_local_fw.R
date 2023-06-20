501a6d38a96268fc05ac5cd96bc83151dd3a5c7a
FMestre1 <fmestre@ruinabeiro.uevora.pt>
2023-06-20 12:21:06 +0100

    removed species

--- 3.analyse_local_fw.R
+++ 3.analyse_local_fw.R
@@ -51,7 +51,7 @@ species_loss <- rep(NA, length(local_fw_MAIORANO_REMOVED))
 connectance_dif <- rep(NA, length(local_fw_MAIORANO_REMOVED))
 compart_dif <- rep(NA, length(local_fw_MAIORANO_REMOVED))
 
-#Go into a for loop
+#LOOP
 for(i in 1:length(local_fw_MAIORANO_REMOVED)){
   
 cheddar1 <- local_fw_MAIORANO[[i]]
@@ -149,6 +149,7 @@ fractions_top_intermediate_basal_nodes <- data.frame(names(local_fw_MAIORANO), m
 names(fractions_top_intermediate_basal_nodes) <- c("grid", "BEFORE_top_level", "BEFORE_Interm_level", "BEFORE_basal_level", 
                                                    "AFTER__top_level", "AFTER_Interm_level", "AFTER_basal_level", "changed?") 
 
+#LOOP
 for(i in 1:length(local_fw_MAIORANO)){
 
 before_net <- local_fw_MAIORANO[[i]]
@@ -179,3 +180,143 @@ head(fractions_top_intermediate_basal_nodes)
 
 #Save
 #save(fractions_top_intermediate_basal_nodes, file = "fractions_top_intermediate_basal_nodes.RData")
+
+
+################################################################################
+# Boxplots of the fraction of top, intermediate, and basal nodes
+################################################################################
+
+library(ggplot2)
+
+head(fractions_top_intermediate_basal_nodes)
+View(fractions_top_intermediate_basal_nodes)
+
+head(fractions_top_intermediate_basal_nodes[,1:4])
+head(fractions_top_intermediate_basal_nodes[,c(1,5,6,7)])
+
+
+before_nt <- fractions_top_intermediate_basal_nodes[,1:4]
+after_nt <- fractions_top_intermediate_basal_nodes[,c(1,5,6,7)]
+
+names(before_nt)[-1] <- c("top", "mid", "basal")
+names(after_nt)[-1] <- c("top", "mid", "basal")
+
+before_nt <- data.frame(before_nt, "before")
+after_nt <- data.frame(after_nt, "after")
+
+names(before_nt)[5] <- "before_after"
+names(after_nt)[5] <- "before_after"
+
+head(before_nt)
+head(after_nt)
+
+net_changes <- rbind(before_nt, after_nt)
+net_changes$before_after <- as.factor(net_changes$before_after)
+
+#Reorder factor levels
+net_changes$before_after <- factor(net_changes$before_after,     
+                                   c("before", "after"))
+### Boxplot ###
+names(net_changes)
+str(net_changes)
+
+#TOP
+top_box <- ggplot(net_changes, aes(x = before_after, y = top))
+
+top_box2 <- top_box + geom_boxplot(aes(fill = before_after),) +
+  scale_fill_manual(values = c("#999999", "#E69F00"))
+
+top_box2
+
+#MID
+#TOP
+mid <- ggplot(net_changes, aes(x = before_after, y = mid))
+
+mid2 <- mid + geom_boxplot(aes(fill = before_after),) +
+  scale_fill_manual(values = c("#999999", "#E69F00"))
+
+mid2
+
+#BASAL
+#TOP
+basal <- ggplot(net_changes, aes(x = before_after, y = basal))
+
+basal2 <- basal + geom_boxplot(aes(fill = before_after),) +
+  scale_fill_manual(values = c("#999999", "#E69F00"))
+
+basal2
+
+################################################################################
+# Where are the removed species, in which trophic level? 
+################################################################################
+
+removed_position <- data.frame(names(local_fw_MAIORANO), matrix(ncol = 4, nrow = length(local_fw_MAIORANO)))
+names(removed_position) <- c("grid", "top_level", "interm_level", "basal_level", "changed?") 
+
+head(removed_position)
+
+#LOOP
+for(i in 1:length(local_fw_MAIORANO_REMOVED)){
+  
+  before_net <- local_fw_MAIORANO[[i]]
+  after_net <- local_fw_MAIORANO_REMOVED[[i]]
+  
+  if(!is.na(before_net) && !is.na(after_net)) {
+  
+  removed_species <- before_net$nodes$node[!(before_net$nodes$node %in% after_net$nodes$node)]
+  
+  if(length(removed_species)){
+    
+    tp1 <- cheddar::TopLevelNodes(before_net)
+    it1 <- cheddar::IntermediateNodes(before_net)
+    bs1 <- cheddar::BasalNodes(before_net)
+    
+    removed_position$top_level[i] <- sum(removed_species %in% tp1)/length(removed_species)
+    removed_position$interm_level[i] <- sum(removed_species %in% it1)/length(removed_species)
+    removed_position$basal_level[i] <- sum(removed_species %in% bs1)/length(removed_species)
+    
+    removed_position$`changed?`[i] <- "yes"
+    
+  } else removed_position$`changed?`[i] <- "no"
+
+}
+  
+message(i)
+
+}
+
+#Save
+#save(removed_position, file = "removed_position.RData")
+
+
+################################################################################
+# Plot it...
+################################################################################
+
+top_rm <- data.frame(removed_position$top_level, "top")
+mid_rm <- data.frame(removed_position$interm_level, "mid")
+basal_rm <- data.frame(removed_position$basal_level, "basal")
+
+names(top_rm) <- c("proportion", "level")
+names(mid_rm) <- c("proportion", "level")
+names(basal_rm) <- c("proportion", "level")
+
+head(top_rm)
+head(mid_rm)
+head(basal_rm)
+
+removed_position <- rbind(top_rm, mid_rm, basal_rm)
+
+removed_position$level <- as.factor(removed_position$level)
+
+#Reorder factor levels
+removed_position$level <- factor(removed_position$level,     
+                                 c("top", "mid", "basal"))
+
+rem_tp <- ggplot(removed_position, aes(x = level, y = proportion))
+
+rem_tp2 <- rem_tp + geom_boxplot(aes(fill = level),) +
+  scale_fill_manual(values = c("#999999", "#E69F00", "#E80F00"))
+
+rem_tp2
+
