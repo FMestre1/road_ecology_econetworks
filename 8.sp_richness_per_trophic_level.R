107f703735fcb33a0a95b1883da813d2ded29c93
FMestre1 <mestre.frederico@gmail.com>
2023-09-16 19:02:00 +0100

    16-09-2023

--- 8.sp_richness_per_trophic_level.R
+++ 8.sp_richness_per_trophic_level.R
@@ -40,3 +40,65 @@ grids_grilo_shape <- terra::vect("C:\\Users\\asus\\Documents\\0. Artigos\\roads_
 #terra::plet(sp_richness_per_trophic_level, "intermediate")
 #terra::plet(sp_richness_per_trophic_level, "basal")
 
+################################################################################
+#               Creating map o relative loss of interactions 
+################################################################################
+#FMestre
+#16-09-2023
+
+local_fw_MAIORANO #the initial FW structures
+local_fw_MAIORANO_REMOVED_PRIMARY_EX #priamary extinctions
+local_fw_MAIORANO_REMOVED #cascading effect
+
+#Primary
+nr_lost_interactions_prim_RELATIVE <- data.frame(matrix(nrow=length(local_fw_MAIORANO_REMOVED_PRIMARY_EX), ncol = 2))
+names(nr_lost_interactions_prim_RELATIVE) <- c("grid","lost_interactions_relative")
+head(nr_lost_interactions_prim_RELATIVE)
+
+for(i in 1:nrow(nr_lost_interactions_prim_RELATIVE)){
+  
+  if(any(!is.na(local_fw_MAIORANO[[i]]))){
+    nr_lost_interactions_prim_RELATIVE[i,1] <- local_fw_MAIORANO[[i]]$properties$title
+    if(!is.null(nrow(local_fw_MAIORANO[[i]]$trophic.links))) {
+      initial_int <- nrow(local_fw_MAIORANO[[i]]$trophic.links)
+      final_int <- nrow(local_fw_MAIORANO_REMOVED_PRIMARY_EX[[i]]$trophic.links)
+      
+      nr_lost_interactions_prim_RELATIVE[i,2] <- ((initial_int - final_int) * 100)/initial_int
+        
+    } else nr_lost_interactions_prim_RELATIVE[i,2]<- 0
+}
+  
+}
+
+#lost_interactions_with_primary_extinctions_RELATIVE <- merge(x=grids_grilo_shape, y=nr_lost_interactions_prim_RELATIVE, by.x="PageName", by.y="grid")
+#terra::writeVector(lost_interactions_with_primary_extinctions_RELATIVE, "lost_interactions_with_primary_extinctions_RELATIVE.shp")
+terra::plet(lost_interactions_with_primary_extinctions_RELATIVE, "lost_interactions_relative")
+
+
+#Cascading
+nr_lost_interactions_cascading_RELATIVE <- data.frame(matrix(nrow=length(local_fw_MAIORANO), ncol = 2))
+names(nr_lost_interactions_cascading_RELATIVE) <- c("grid","lost_interactions_relative")
+head(nr_lost_interactions_cascading_RELATIVE)
+
+
+for(i in 1:nrow(nr_lost_interactions_cascading_RELATIVE)){
+  
+  if(any(!is.na(local_fw_MAIORANO[[i]]))){
+    nr_lost_interactions_cascading_RELATIVE[i,1] <- local_fw_MAIORANO[[i]]$properties$title
+    if(!is.null(nrow(local_fw_MAIORANO[[i]]$trophic.links)))
+      {
+      
+    initial_int2 <- nrow(local_fw_MAIORANO[[i]]$trophic.links)
+    final_int2 <- nrow(local_fw_MAIORANO_REMOVED[[i]]$trophic.links)
+      
+    nr_lost_interactions_cascading_RELATIVE[i,2] <- ((initial_int2 - final_int2) * 100)/initial_int2
+      
+      } else nr_lost_interactions_cascading_RELATIVE[i,2] <- 0
+  }
+  
+}
+
+#lost_interactions_with_sec_extinctions_RELATIVE <- merge(x=grids_grilo_shape, y=nr_lost_interactions_cascading_RELATIVE, by.x="PageName", by.y="grid")
+#terra::writeVector(lost_interactions_with_sec_extinctions_RELATIVE, "lost_interactions_with_sec_extinctions_RELATIVE.shp")
+#terra::plet(lost_interactions_with_sec_extinctions_RELATIVE, "lost_interactions_relative")
+
