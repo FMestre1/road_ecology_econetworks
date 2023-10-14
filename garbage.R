94124ebb36c3150c54630cfd7823b6aa7363aa78
FMestre1 <mestre.frederico@gmail.com>
2023-10-14 10:31:19 +0100

    14-10-2023

--- garbage.R
+++ garbage.R
@@ -480,3 +480,467 @@ par(mfrow=c(1,3))
 plot(local_fw_MAIORANO[["4458"]], main = "CU14")
 plot(local_fw_MAIORANO_REMOVED_PRIMARY_EX[["4458"]], main = "CU14")
 plot(local_fw_MAIORANO_REMOVED[["4458"]], main = "CU14")
+
+
+################################################################################
+################################################################################
+#           SCRIPT 2 - FIGURES
+################################################################################
+################################################################################
+
+#FMestre
+#September 2023
+
+#Load packages
+library(ggplot2)
+library(terra)
+
+################################################################################
+#    New figure with the absolute number of species per trophic level
+################################################################################
+
+names(extinctions_levels2)
+str(extinctions_levels2)
+colMeans(extinctions_levels2[,-1])
+
+top_start <- data.frame(extinctions_levels2$BEFORE_top_level, "top_start")
+mid_start <- data.frame(extinctions_levels2$BEFORE_interm_level, "intermediate_start")
+basal_start <- data.frame(extinctions_levels2$BEFORE_basal_level, "basal_start")
+#
+top_prim <- data.frame(extinctions_levels2$PRIM_top_level, "top_prim")
+mid_prim <- data.frame(extinctions_levels2$PRIM_interm_level, "intermediate_prim")
+basal_prim <- data.frame(extinctions_levels2$PRIM_basal_level, "basal_prim")
+#
+top_sec <- data.frame(extinctions_levels2$SEC_top_level, "top_sec")
+mid_sec <- data.frame(extinctions_levels2$SEC_interm_level, "intermediate_sec")
+basal_sec <- data.frame(extinctions_levels2$SEC_basal_level, "basal_sec")
+
+names(top_start) <- c("species", "level_step")
+names(mid_start) <- c("species", "level_step")
+names(basal_start) <- c("species", "level_step")
+#
+names(top_prim) <- c("species", "level_step")
+names(mid_prim) <- c("species", "level_step")
+names(basal_prim) <- c("species", "level_step")
+#
+names(top_sec) <- c("species", "level_step")
+names(mid_sec) <- c("species", "level_step")
+names(basal_sec) <- c("species", "level_step")
+
+species_tl_step <- rbind(top_start, mid_start, basal_start,
+                         top_prim,mid_prim,basal_prim,
+                         top_sec,mid_sec,basal_sec)
+
+species_tl_step$level_step <- as.factor(species_tl_step$level_step)
+
+unique(species_tl_step$level_step)
+
+#Reorder factor levels
+species_tl_step$level_step <- factor(species_tl_step$level_step,     
+                                     c("top_start", "intermediate_start", "basal_start",
+                                       "top_prim", "intermediate_prim", "basal_prim",
+                                       "top_sec", "intermediate_sec", "basal_sec")
+)
+
+species_tl_step_plot <- ggplot(species_tl_step, 
+                               aes(x = level_step, y = species))
+
+species_tl_step_plot2 <- species_tl_step_plot + geom_violin(aes(fill = level_step)) +
+  ylab("Number of Species per Trophic Level") +
+  xlab("Trophic level - Time step") +
+  scale_fill_manual(values = c("#E70F00", "#E69F00", "#1E811E",
+                               "#E70F00", "#E69F00", "#1E811E",
+                               "#E70F00", "#E69F00", "#1E811E")) +
+  ggtitle("Number of Species")
+
+species_tl_step_plot2
+
+################################################################################
+#                             Plot per trophic level
+################################################################################
+
+#levels(species_tl_step$level_step)
+
+top_tl_species <- species_tl_step[species_tl_step$level_step %in% c("top_start", "top_prim" , "top_sec"), ]      
+mid_tl_species <- species_tl_step[species_tl_step$level_step %in% c("intermediate_start", "intermediate_prim" , "intermediate_sec"), ]      
+basal_tl_species <- species_tl_step[species_tl_step$level_step %in% c("basal_start", "basal_prim" , "basal_sec"), ]      
+
+# TOP
+top_plot <- ggplot(top_tl_species, aes(x = level_step, y = species))
+
+top_plot_plot2 <- top_plot + geom_violin(aes(fill = level_step)) +
+  ylab("Number of Species - Top Trophic level") +
+  xlab("Time step") +
+  scale_fill_manual(values = c("#C19235","#82BC48", "#DCCD3D")) +
+  ggtitle("Number of Species")
+
+top_plot_plot2
+
+# MID
+mid_plot <- ggplot(mid_tl_species, aes(x = level_step, y = species))
+
+mid_plot_plot2 <- mid_plot + geom_violin(aes(fill = level_step)) +
+  ylab("Number of Species - Intermediate Trophic level") +
+  xlab("Time step") +
+  scale_fill_manual(values = c("#C19235","#82BC48", "#DCCD3D")) +
+  ggtitle("Number of Species")
+
+mid_plot_plot2
+
+#BASAL
+basal_plot <- ggplot(basal_tl_species, aes(x = level_step, y = species))
+
+basal_plot_plot2 <- basal_plot + geom_violin(aes(fill = level_step)) +
+  ylab("Number of Species - Basal Trophic level") +
+  xlab("Time step") +
+  scale_fill_manual(values = c("#C19235","#82BC48", "#DCCD3D")) +
+  ggtitle("Number of Species")
+
+basal_plot_plot2
+
+
+################################################################################
+#                              Plot removed species
+################################################################################
+
+#using:
+#extinctions_levels2
+
+lost_species <- data.frame(names(local_fw_MAIORANO), matrix(ncol = 6, nrow = length(local_fw_MAIORANO)))
+names(lost_species) <- c("grid", 
+                         "BEFORE_to_PRIMARY_top_level", 
+                         "BEFORE_to_PRIMARY_interm_level", 
+                         "BEFORE_to_PRIMARY_basal_level", 
+                         "PRIMARY_to_CASCADING_top_level",
+                         "PRIMARY_to_CASCADING_interm_level",
+                         "PRIMARY_to_CASCADING_basal_level"
+) 
+#View(lost_species)
+
+for(i in 1:nrow(lost_species)){
+  
+  tp_0 <- extinctions_levels2[i,2]
+  it_0 <- extinctions_levels2[i,3]
+  bs_0 <- extinctions_levels2[i,4]
+  tp_1 <- extinctions_levels2[i,5]
+  it_1 <- extinctions_levels2[i,6]
+  bs_1 <- extinctions_levels2[i,7]
+  tp_2 <- extinctions_levels2[i,8]
+  it_2 <- extinctions_levels2[i,9]
+  bs_2 <- extinctions_levels2[i,10]
+  
+  lost_species$BEFORE_to_PRIMARY_top_level[i] <- tp_0-tp_1
+  lost_species$BEFORE_to_PRIMARY_interm_level[i] <- it_0-it_1
+  lost_species$BEFORE_to_PRIMARY_basal_level[i] <- bs_0-bs_1
+  
+  lost_species$PRIMARY_to_CASCADING_top_level[i] <- tp_1-tp_2
+  lost_species$PRIMARY_to_CASCADING_interm_level[i] <- it_1-it_2
+  lost_species$PRIMARY_to_CASCADING_basal_level[i] <- bs_1-bs_2 
+  
+  
+  message(i)
+  
+}
+
+#View(lost_species)
+
+#1. Original to primary #####
+top_orig_prim3 <- data.frame(lost_species$BEFORE_to_PRIMARY_top_level, "top")
+mid_orig_prim3 <- data.frame(lost_species$BEFORE_to_PRIMARY_interm_level, "intermediate")
+basal_orig_prim3 <- data.frame(lost_species$BEFORE_to_PRIMARY_basal_level, "basal")
+
+names(top_orig_prim3) <- c("species", "level")
+names(mid_orig_prim3) <- c("species", "level")
+names(basal_orig_prim3) <- c("species", "level")
+
+removed_position_orig_prim3 <- rbind(top_orig_prim3, mid_orig_prim3, basal_orig_prim3)
+
+removed_position_orig_prim3$level <- as.factor(removed_position_orig_prim3$level)
+
+#Reorder factor levels
+removed_position_orig_prim3$level <- factor(removed_position_orig_prim3$level,     
+                                            c("top", "intermediate", "basal"))
+
+data_means_orig_prim <- aggregate(removed_position_orig_prim3$species,
+                                  list(removed_position_orig_prim3$level),
+                                  mean)
+
+rem_orig_prim3 <- ggplot(removed_position_orig_prim3, 
+                         aes(x = level, y = species))
+
+rem_orig_prim3_violin <- rem_orig_prim3 + geom_boxplot(aes(fill = level)) +
+  ylab("Number of species") +
+  xlab("Trophic level") +
+  scale_fill_manual(values = c("#E70F00", "#E69F00", "#1E811E")) +
+  ggtitle("Primary extinctions") +
+  stat_summary(fun = mean, geom = "point", col = "black") +
+  stat_summary(fun = mean, geom = "text", col = "black",
+               vjust = 1.5, aes(label = round(..y.., digits = 1)))
+
+rem_orig_prim3_violin
+
+#2. Primary to Cascading #####
+top_prim_sec3 <- data.frame(lost_species$PRIMARY_to_CASCADING_top_level, "top")
+mid_prim_sec3 <- data.frame(lost_species$PRIMARY_to_CASCADING_interm_level, "intermediate")
+basal_prim_sec3 <- data.frame(lost_species$PRIMARY_to_CASCADING_basal_level, "basal")
+
+names(top_prim_sec3) <- c("species", "level")
+names(mid_prim_sec3) <- c("species", "level")
+names(basal_prim_sec3) <- c("species", "level")
+
+removed_position_prim_sec3 <- rbind(top_prim_sec3, mid_prim_sec3, basal_prim_sec3)
+
+removed_position_prim_sec3$level <- as.factor(removed_position_prim_sec3$level)
+
+#Reorder factor levels
+removed_position_prim_sec3$level <- factor(removed_position_prim_sec3$level,     
+                                           c("top", "intermediate", "basal"))
+
+data_means_prim_sec <- aggregate(removed_position_prim_sec3$species,
+                                 list(removed_position_prim_sec3$level),
+                                 mean)
+
+rem_prim_sec3 <- ggplot(removed_position_prim_sec3, 
+                        aes(x = level, y = species)) 
+
+text(x = 1:nrow(data_means_prim_sec),
+     y = 10,
+     labels = paste("Mean:", round(data_means_prim_sec$x, 1)),
+     col = "black")
+
+rem_prim_sec3_violin <- rem_prim_sec3 + geom_boxplot(aes(fill = level)) +
+  ylab("Number of species") +
+  xlab("Trophic level") +
+  scale_fill_manual(values = c("#E70F00", "#E69F00", "#1E811E")) +
+  ggtitle("Cascading effect") +
+  stat_summary(fun = mean, geom = "point", col = "black") +
+  stat_summary(fun = mean, geom = "text", col = "black",
+               vjust = 1.5, aes(label = round(..y.., digits = 1)))
+
+rem_prim_sec3_violin
+
+#Combine three plots for fig.2
+grid.arrange(rem_orig_prim3_violin, 
+             rem_prim_sec3_violin, 
+             nrow = 2)
+
+
+################################################################################
+################################################################################
+#               SCRIPT 7 - TROPHIC LEVEL OF MOST VULNERABLE SPECIES
+################################################################################
+################################################################################
+
+#FMestre
+#28-09-2023
+
+#Load packages
+library(cheddar)
+library(ggplot2)
+library(dplyr)
+
+vuln_tl_table <- data.frame(matrix(nrow = length(local_fw_MAIORANO), ncol = 3))
+names(vuln_tl_table) <- c("top_vuln", "intermediate_vuln", "basal_vuln")
+#head(vuln_tl_table)
+
+for(j in 1:length(local_fw_MAIORANO)){
+  
+  if(unique(!is.na(local_fw_MAIORANO[[j]])))
+    
+  {
+    
+    tp_level <- as.vector(cheddar::IsTopLevelNode(local_fw_MAIORANO[[j]]))
+    interm_level <- as.vector(cheddar::IsIntermediateNode(local_fw_MAIORANO[[j]]))
+    basal_level <- as.vector(cheddar::IsBasalNode(local_fw_MAIORANO[[j]]))
+    
+    df1 <- data.frame(cheddar::NPS(local_fw_MAIORANO[[j]]), 
+                      tp_level,
+                      interm_level,
+                      basal_level)
+    
+    df1$tp_level[df1$tp_level] <- "top"                  
+    df1$interm_level[df1$interm_level] <- "intermediate"
+    df1$basal_level[df1$basal_level] <- "basal"
+    
+    tl <- rep(NA, nrow(df1))
+    
+    for(i in 1:nrow(df1)){
+      
+      if(df1$tp_level[i] != FALSE) tl[i] <- "top"
+      if(df1$interm_level[i] != FALSE) tl[i] <- "intermediate"
+      if(df1$basal_level[i] != FALSE) tl[i] <- "basal"
+      if(df1$tp_level[i] == FALSE & df1$interm_level[i] == FALSE & df1$basal_level[i] == FALSE) tl[i] <- "none"
+      
+      
+    }
+    
+    df2 <- data.frame(cheddar::NPS(local_fw_MAIORANO[[j]]), tl)
+    
+    df2$tl <- as.factor(df2$tl)
+    
+    df2 <- df2[,c(1,2,7)]
+    
+    df3 <- as.data.frame(df2 %>% group_by (tl) %>% summarise(Average=mean(Median_MAXroad.RM.1000.)))
+    
+    vuln_tl_table$top_vuln[j] <- df3[3,2]
+    vuln_tl_table$intermediate_vuln[j] <- df3[2,2]
+    vuln_tl_table$basal_vuln[j] <- df3[1,2]
+    
+  }
+  
+  message(j)
+  
+}
+
+#nrow(vuln_tl_table)
+#str(vuln_tl_table)
+#head(vuln_tl_table)
+
+top1 <- data.frame(vuln_tl_table$top_vuln, rep("top", nrow(vuln_tl_table)))
+int1 <- data.frame(vuln_tl_table$intermediate_vuln, rep("intermediate", nrow(vuln_tl_table)))
+bas1 <- data.frame(vuln_tl_table$basal_vuln, rep("basal", nrow(vuln_tl_table)))
+
+names(top1) <- c("vulnerability", "tl")
+names(int1) <- c("vulnerability", "tl")
+names(bas1) <- c("vulnerability", "tl")
+
+vuln_tl_table <- rbind(top1, int1, bas1)
+vuln_tl_table$tl <- as.factor(vuln_tl_table$tl)
+
+vuln_tl_table$tl <- factor(vuln_tl_table$tl, 
+                           levels = c("top", "intermediate", "basal", "none")
+)
+
+tl_vuln <- ggplot(vuln_tl_table, aes(x = tl, y = vulnerability))
+
+tl_vuln2 <- tl_vuln + geom_violin(aes(fill = tl),) +
+  ylab("vulnerability") +
+  xlab("trophic level") +
+  scale_fill_manual(values = c("#E70F00", "#E69F00", "#1E811E"))
+
+
+tl_vuln2 + labs(fill = "trophic level")
+
+#save(vuln_tl_table, file = "vuln_tl_table_fig2a_NEW.RData")
+
+# Compute an analysis of variance
+vulnerability_tl_aov <- aov(vulnerability ~ tl, data = vuln_tl_table)
+# Summary of the analysis
+summary(vulnerability_tl_aov)
+
+
+################################################################################
+################################################################################
+#                 SCRIPT 8 - SPECIES RICHNNESS PER TROPHIC LEVEL 
+################################################################################
+################################################################################
+
+#FMestre
+#30-09-2023
+
+#Loading packages
+library(cheddar)
+library(terra)
+library(dplyr)
+library(tidyverse)
+
+#Having
+local_fw_MAIORANO #the initial FW structures
+
+nr_species_per_grid_per_tl <- data.frame(matrix(nrow=length(local_fw_MAIORANO), ncol = 7))
+names(nr_species_per_grid_per_tl) <- c("grid", "total_richness","top", "intermediate", "basal", "connected", "not_connected")
+head(nr_species_per_grid_per_tl)
+
+for(i in 1:nrow(nr_species_per_grid_per_tl)){
+  
+  if(any(!is.na(local_fw_MAIORANO[[i]]))){
+    nr_species_per_grid_per_tl[i,1] <- local_fw_MAIORANO[[i]]$properties$title #Name
+    nr_species_per_grid_per_tl[i,2] <- nrow(local_fw_MAIORANO[[i]]$nodes) #Total Richness
+    nr_species_per_grid_per_tl[i,3] <- length(cheddar::TopLevelNodes(local_fw_MAIORANO[[i]])) #Nr of top level species
+    nr_species_per_grid_per_tl[i,4] <- length(cheddar::IntermediateNodes(local_fw_MAIORANO[[i]])) #Nr of mid level species
+    nr_species_per_grid_per_tl[i,5] <- length(cheddar::BasalNodes(local_fw_MAIORANO[[i]])) #Nr of basal level species
+    nr_species_per_grid_per_tl[i,6] <- length(cheddar::ConnectedNodes(local_fw_MAIORANO[[i]])) #Connected nodes
+    nr_species_per_grid_per_tl[i,7] <- nrow(local_fw_MAIORANO[[i]]$nodes) - length(cheddar::ConnectedNodes(local_fw_MAIORANO[[i]])) #Non connected nodes
+    
+  }
+  message(i)
+}
+
+#View(nr_species_per_grid_per_tl)
+
+#saving as shapefile
+sp_richness_per_trophic_level <- merge(x=grids_grilo_shape, y=nr_species_per_grid_per_tl, by.x="PageNumber", by.y="grid")
+#terra::writeVector(sp_richness_per_trophic_level, "sp_richness_per_trophic_level_11OUT23.shp")
+#terra::plet(sp_richness_per_trophic_level, "top")
+#terra::plet(sp_richness_per_trophic_level, "intermediate")
+#terra::plet(sp_richness_per_trophic_level, "basal")
+
+################################################################################
+#             1. Creating map o relative loss of interactions 
+################################################################################
+
+local_fw_MAIORANO #the initial FW structures
+local_fw_MAIORANO_REMOVED_PRIMARY_EX #priamary extinctions
+local_fw_MAIORANO_REMOVED #cascading effect
+
+#Primary
+nr_lost_interactions_prim_RELATIVE <- data.frame(matrix(nrow=length(local_fw_MAIORANO_REMOVED_PRIMARY_EX), ncol = 2))
+names(nr_lost_interactions_prim_RELATIVE) <- c("grid","lost_interactions_relative")
+#head(nr_lost_interactions_prim_RELATIVE)
+
+for(i in 1:nrow(nr_lost_interactions_prim_RELATIVE)){
+  
+  if(any(!is.na(local_fw_MAIORANO[[i]]))){
+    nr_lost_interactions_prim_RELATIVE[i,1] <- local_fw_MAIORANO[[i]]$properties$title
+    if(!is.null(nrow(local_fw_MAIORANO[[i]]$trophic.links))) {
+      initial_int <- nrow(local_fw_MAIORANO[[i]]$trophic.links)
+      final_int <- nrow(local_fw_MAIORANO_REMOVED_PRIMARY_EX[[i]]$trophic.links)
+      
+      nr_lost_interactions_prim_RELATIVE[i,2] <- ((initial_int - final_int) * 100)/initial_int
+      
+    } else nr_lost_interactions_prim_RELATIVE[i,2]<- 0
+  }
+  
+}
+
+lost_interactions_with_primary_extinctions_RELATIVE <- merge(x=grids_grilo_shape, y=nr_lost_interactions_prim_RELATIVE, by.x="PageNumber", by.y="grid")
+#terra::writeVector(lost_interactions_with_primary_extinctions_RELATIVE, "lost_interactions_with_primary_extinctions_RELATIVE_11OUT23.shp")
+#terra::plet(lost_interactions_with_primary_extinctions_RELATIVE, "lost_interactions_relative")
+
+
+#Cascading
+nr_lost_interactions_cascading_RELATIVE <- data.frame(matrix(nrow=length(local_fw_MAIORANO), ncol = 2))
+names(nr_lost_interactions_cascading_RELATIVE) <- c("grid","lost_interactions_relative")
+#head(nr_lost_interactions_cascading_RELATIVE)
+
+for(i in 1:nrow(nr_lost_interactions_cascading_RELATIVE)){
+  
+  if(any(!is.na(local_fw_MAIORANO[[i]]))){
+    nr_lost_interactions_cascading_RELATIVE[i,1] <- local_fw_MAIORANO[[i]]$properties$title
+    if(!is.null(nrow(local_fw_MAIORANO[[i]]$trophic.links)))
+    {
+      
+      initial_int2 <- nrow(local_fw_MAIORANO[[i]]$trophic.links)
+      final_int2 <- nrow(local_fw_MAIORANO_REMOVED[[i]]$trophic.links)
+      
+      nr_lost_interactions_cascading_RELATIVE[i,2] <- ((initial_int2 - final_int2) * 100)/initial_int2
+      
+    } else nr_lost_interactions_cascading_RELATIVE[i,2] <- 0
+  }
+  
+}
+
+lost_interactions_with_sec_extinctions_RELATIVE <- merge(x=grids_grilo_shape, y=nr_lost_interactions_cascading_RELATIVE, by.x="PageNumber", by.y="grid")
+#terra::writeVector(lost_interactions_with_sec_extinctions_RELATIVE, "lost_interactions_with_sec_extinctions_RELATIVE_11OUT23.shp")
+#terra::plet(lost_interactions_with_sec_extinctions_RELATIVE, "lost_interactions_relative")
+
+
+
+# Use dplyr to calculate the mean of 'Value' by 'Group'
+result <- tl_positions %>%
+  group_by(group) %>%
+  summarize(Mean_Value = mean(tl, na.rm = TRUE))
+
+# Print the result
+print(result)
+
